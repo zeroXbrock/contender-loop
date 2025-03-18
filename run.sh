9fd4caadec43d48861537ffb5d430c097a60a8b1
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-03-18 11:37:41 -0700

    log for missing PRK, more conservative spam params

--- run.sh
+++ run.sh
@@ -10,11 +10,12 @@ CONTENDER="$CONTENDERPATH/target/debug/contender"
 RPC_URL="$2"
 
 if [ -z "$PRK" ]; then
+  echo "\$PRK not found, using default account"
   PRK="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 fi
 
 echo "using wallet address: $(cast wallet address $PRK)"
 
 while true; do
-  $CONTENDER run fill-block $RPC_URL -p $PRK --sdp -d 20 -i 1 -n 50
+  $CONTENDER run fill-block $RPC_URL -p $PRK --sdp -d 10 -i 1 -n 25
 done
